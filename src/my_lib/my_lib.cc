73eff3f80fb006209c4e28f91ee7c7837886435a
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2023-11-26 05:23:12 +0700

    feat: use dependencies using `FetchContent`

--- src/my_lib/my_lib.cc
+++ src/my_lib/my_lib.cc
@@ -1,7 +1,26 @@
 #include <iostream>
 
+#include <spdlog/spdlog.h>
+#include <nlohmann/json.hpp>
+#include <catch2/catch.hpp>
+
 #include "my_lib.h"
 
 void print_hello_world() {
     std::cout << "Hello, world!" << std::endl;
+
+    std::cout << "JSON lib version: "
+        << NLOHMANN_JSON_VERSION_MAJOR << "."
+        << NLOHMANN_JSON_VERSION_MINOR << "."
+        << NLOHMANN_JSON_VERSION_PATCH << std::endl;
+
+    std::cout << "SPD Log version: "
+        << SPDLOG_VER_MAJOR << "."
+        << SPDLOG_VER_MINOR << "."
+        << SPDLOG_VER_PATCH << std::endl;
+
+    std::cout << "Catch2 version: "
+        << CATCH_VERSION_MAJOR << "."
+        << CATCH_VERSION_MINOR << "."
+        << CATCH_VERSION_PATCH << std::endl;
 }
